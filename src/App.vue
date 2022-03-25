39e18008fd09ec0d07ba5a535d7a8b1b0cfe5f3a
Egor Glebov <batmanvs28@gmail.com>
2022-03-25 18:04:00 +0300

    --all

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!-- eslint-disable max-len -->
+<!-- eslint-disable -->
+  <section class="catalog">
+                <ul class="catalog__list">
+                    <li class="catalog__item" v-for="(product, index) in products" :key="index"><a class="catalog__pic" href="#"><img :src="product.image"
+                               :alt="product.title"></a>
+                        <h3 class="catalog__title"><a href="#">{{product.title}}</a></h3><span
+                            class="catalog__price">{{product.price}} ₽</span>
+                        <ul class="colors colors--black">
+                            <li class="colors__item"><label class="colors__label"><input class="colors__radio sr-only"
+                                        type="radio" name="color-1" value="#73B6EA" checked=""> <span
+                                        class="colors__value" style="background-color: #73B6EA;"></span></label></li>
+                            <li class="colors__item"><label class="colors__label"><input class="colors__radio sr-only"
+                                        type="radio" name="color-1" value="#8BE000"> <span class="colors__value"
+                                        style="background-color: #8BE000;"></span></label></li>
+                            <li class="colors__item"><label class="colors__label"><input class="colors__radio sr-only"
+                                        type="radio" name="color-1" value="#222"> <span class="colors__value"
+                                        style="background-color: #222;"></span></label></li>
+                        </ul>
+                    </li>
+                
+                </ul>
+                <ul class="catalog__pagination pagination">
+                    <li class="pagination__item"><a
+                            class="pagination__link pagination__link--arrow pagination__link--disabled"
+                            aria-label="Предыдущая страница"><svg width="8" height="14" fill="currentColor">
+                                <use xlink:href="#icon-arrow-left"></use>
+                            </svg></a></li>
+                    <li class="pagination__item"><a class="pagination__link pagination__link--current">1</a></li>
+                    <li class="pagination__item"><a class="pagination__link" href="#">2</a></li>
+                    <li class="pagination__item"><a class="pagination__link" href="#">3</a></li>
+                    <li class="pagination__item"><a class="pagination__link" href="#">4</a></li>
+                    <li class="pagination__item"><a class="pagination__link" href="#">...</a></li>
+                    <li class="pagination__item"><a class="pagination__link" href="#">10</a></li>
+                    <li class="pagination__item"><a class="pagination__link pagination__link--arrow" href="#"
+                            aria-label="Следующая страница"><svg width="8" height="14" fill="currentColor">
+                                <use xlink:href="#icon-arrow-right"></use>
+                            </svg></a></li>
+                </ul>
+            </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import products from './data/products';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      products,
+    };
   },
 };
 </script>
-
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
+<!-- eslint-enable max-len -->
+<!-- eslint-enable -->
---
+++ src/data/products.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Смартфон realme C21Y 3/32GB',
+    price: 10790,
+    image: '/img/realmeC21Y.webp',
+  },
+  {
+    title: 'Игровая клавиатура проводная A4Tech Bloody B328',
+    price: 1890,
+    image: '/img/bloodyb328.webp',
+  },
+  {
+    title: 'Электрическая зубная щетка Oral-B Pro 3 3500',
+    price: 4490,
+    image: '/img/oral-b.webp',
+  },
+  {
+    title: 'Беспроводные наушники ADIDAS Z.N.E. 01',
+    price: 10440,
+    image: '/img/adidas.webp',
+  },
+  {
+    title: 'Умные часы HUAWEI WATCH Fit',
+    price: 6990,
+    image: '/img/huaweiWatch.webp',
+  },
+  {
+    title: 'Смартфон Poco M4 Pro 4G 8/256GB',
+    price: 29990,
+    image: '/img/pocoM4.webp',
+  },
+  {
+    title: 'Смартфон Samsung Galaxy A32 4/128GB',
+    price: 31490,
+    image: '/img/samsungA32.webp',
+  },
+  {
+    title: 'Смартфон Xiaomi Redmi Note 11 NFC 4/64GB',
+    price: 23081,
+    image: '/img/redmi11.webp',
+  },
+  {
+    title: 'Смартфон Apple iPhone 12 128GB',
+    price: 77981,
+    image: '/img/iphone12.webp',
+  },
+  {
+    title: 'Наушники внутриканальные Sony MDR-EX15LP, 3.5 мм',
+    price: 790,
+    image: '/img/sonyMDR.webp',
+  },
+  {
+    title: 'Игровые наушники HyperX Cloud Alpha S Blackout',
+    price: 11479,
+    image: '/img/cloudAlpha.webp',
+  },
+];
